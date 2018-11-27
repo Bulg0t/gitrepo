e27c6049dcd8fb9eaba0c209588faa3ffcb7cfa8
Bulg0t <michal.potocki@lo1.sandomierz.pl>
2018-11-27 13:30:50 +0100

    Funkcje

--- python/lotek.py
+++ python/lotek.py
@@ -15,18 +15,22 @@ def main(args):
         liczba = random.randint(0, 10)
         if not liczby.count(liczba):
             liczby.append(liczba)
-            ileliczb -= 1 #dekrementacja o 1
-    print(liczby)
-    
+            ileliczb -= 1 # dekrementacja o 1
+    #print(liczby)
     ileliczb = len(liczby)
     typy = set()
     while ileliczb:
-        typ = input('Podaj typ: ')
+        typ = int(input('Podaj typ: '))
         if typ not in typy:
             typy.add(typ)
-            ileliczb -= 1 #dekrementacja o 1
-            
-    print(typy)
+            ileliczb -= 1 # dekrementacja o 1
+    #print(typy)
+    trafione = set(liczby) & typy
+    if trafione:
+        print("Trafione: ", trafione)
+    else:
+        print("Spróbuj jeszcze raz!")
+    
     
     
     return 0
