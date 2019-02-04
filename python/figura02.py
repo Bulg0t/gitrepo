6fc2b270bceeb0dce249c5a8e8e8ab183e22ed9e
Bulg0t <michal.potocki@lo1.sandomierz.pl>
2019-02-04 12:39:00 +0100

    Kalkulator

--- python/figura02.py
+++ python/figura02.py
@@ -12,7 +12,7 @@ def main(args):
         turtle.forward(100)
         turtle.right(144)
     for i in range(5):
-        turtle.forward(200)
+        turtle.forward(300)
         turtle.right(144)
     turtle.done()
     return 0
---
+++ python/kalkulator.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  kalkulator.py
+
+def pokaz_liste():
+    print('''Lista działań:
+          +  – dodawanie
+          -  – odejmowanie
+          *  – mnożenie
+          /  – dzielenie
+          // – dzielenie całkowite
+          %  – dzielenie modulo
+          ^  – potęgowanie
+          !  – silnia
+          sin – sinus
+          cos – cosinus
+          koniec – wyjście z programu
+          l – pokaż listę
+          ''')
+
+
+def pobierz_liczbe(komunikat='Pobierz liczbę: '):
+    a = input(komunikat)
+    if a.isdigit():
+        return int(a)
+    return False
+    
+def dziel(a, b):
+    if b != 0:
+        return a / b
+    else:
+        print('Błąd dzielenia przez zero!')
+        return False
+
+def main(args):
+    pokaz_liste()
+    while True:
+        d = input(''' Wybierz działanie ''')
+        if d == '+':
+            pass
+        elif d == '-':
+            pass
+        elif d == '*':
+            pass
+        elif d == '//':
+            pass
+        elif d == '/':
+            a = pobierz_liczbe( 'Podaj dzielną: ')
+            b = pobierz_liczbe( 'Podaj dzielnik: ') 
+            if a and b:
+                wynik = dziel(a, b)
+                if wynik:
+                    print('{} / {} = {}' .format(a ,b, wynik))
+        elif d == '%':
+            pass 
+        elif d == '^':
+            pass 
+        elif d == '!':
+            pass 
+        elif d == 'sin':
+            pass 
+        elif d == 'cos':
+            pass 
+        elif d == 'l':
+            pokaz_liste()
+        elif d == 'koniec':
+            pass 
+        else:
+            print('Błędny wybór!')
+
+
+
+    return 0
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
