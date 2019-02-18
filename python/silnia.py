f6c7bafb099778308dc11f74a9ecc1fb41cc2c98
Bulg0t <michal.potocki@lo1.sandomierz.pl>
2019-02-18 12:36:07 +0100

    rekurencja

--- python/silnia.py
+++ python/silnia.py
@@ -13,13 +13,28 @@ def silnia_it(n):
         wynik = wynik * i
     return wynik
     
+def silnia_rek(n):
+    # n! = (n - 1)! * n
+    # 4! = 3! * 4
+    # 3! = 2! * 3
+    # 2! = 1! * 2
+    # 1! = 0! * 1
+    # 0! = 1
+    # 1! = 1 * 1
+    # 2! = 1 * 2
+    # 3! = 2 * 3
+    # 4! = 6 * 4
+    if n == 0:
+        return 1
+    return silnia_rek(n - 1) * n
+    
 def main(args):
-    assert silnia_it(5) == 120
-    assert silnia_it(1) == 1
-    assert silnia_it(4) == 24
-    assert silnia_it(0) == 1
+    assert silnia_rek(5) == 120
+    assert silnia_rek(1) == 1
+    assert silnia_rek(4) == 24
+    assert silnia_rek(0) == 1
     n = int(input('Podaj liczbę naturalną : '))
-    print("{}! = {}" .format(n, silnia_it(n)))
+    print("{}! = {}" .format(n, silnia_rek(n)))
 
     return 0
 
