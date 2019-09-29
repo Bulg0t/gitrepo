10d83365cd573ff8bd199225b9bb08558e711fa0
Bulg0t <michal.potocki@lo1.sandomierz.pl>
2019-09-29 23:15:51 +0200

    petle-cwiczenia

---
+++ cpp/petla4.cpp
@@ -0,0 +1,18 @@
+
+/*
+ * petla_cw4.cpp
+ *
+ */
+
+
+#include <iostream>
+using namespace std;
+int main(int argc, char **argv)
+{
+    int i =10;
+    for(int i=10; i<100; i++)
+    if (i%3 == 0){
+     { cout << i << endl; }
+}
+	return 0;
+}
