a6442c4cb2361b8b4a38ac3cddb86e22254da387
Bulg0t <michal.potocki@lo1.sandomierz.pl>
2019-10-02 09:40:51 +0200

    parsek

--- public_html/klasa2a/formularz.php
+++ public_html/klasa2a/formularz.php
@@ -10,7 +10,7 @@
 </head>
 
 <body>
-    <nav class="navbar navbar-expand-sm fixed-top bg-dark navbar-dark">
+    <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
         <a class="navbar-brand" href="http://lo1.sandomierz.pl">I LO CG</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
             <span class="navbar-toggler-icon"></span>
@@ -28,13 +28,29 @@
     </nav>
     <div class="container-fluid">
         <div class="row">
+            <div class="col">
+                <?php
+
+                echo '<h2>Przetwarzanie formularza</h2>';
+                print_r($_POST);
+                print_r(isset($_POST['login']));
+                if (isset($_POST['login']))
+                echo '<p>Witaj '.$_POST['login'].'</p>';
+                else
+                    echo '<p>Zaloguj się</p>';
+                ?>
+
+            </div>
+
+        </div>
+        <div class="row">
             <div class="col-3">&nbsp;</div>
             <div class="col">
                 <h1 class="text-center">
                     Formularz<small>– w Bootstrapie</small>
                 </h1>
                 <hr>
-                <form action="formularz.html" method="POST" name="dane" id="dane">
+                <form action="formularz.php" method="POST" name="dane" id="dane">
                     <input type="hidden" name="id_user" value="10">
                     <div class="form-group">
                         <label for="login">Login:</label>
