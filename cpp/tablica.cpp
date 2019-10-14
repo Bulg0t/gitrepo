561fafbced7fc16d519d4cf26e78f0711f41f227
Bulg0t <michal.potocki@lo1.sandomierz.pl>
2019-10-14 13:22:06 +0200

    tablica

--- cpp/tablica.cpp
+++ cpp/tablica.cpp
@@ -12,14 +12,14 @@ int main(int argc, char **argv)
 	int rozmiar = 5;
 	int tablica[rozmiar];
 	int i = 0;
-	cout << "Podaj 20 liczb"<<endl;
+	cout << "Podaj 5 liczb"<<endl;
 	for(i = 0;i<rozmiar;i++){
 		cin >> tablica[i];
 		}
 	for(i = 0;i<rozmiar;i++){
-	cout << tablica[i]<<" ";
+	cout << tablica[rozmiar - (i+1)]<<" ";
 		}
-	
+
 	return 0;
 }
 
