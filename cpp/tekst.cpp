919d17fd7d1f3234af15de577d6d319feab680b0
Bulg0t <michal.potocki@lo1.sandomierz.pl>
2019-11-03 14:10:57 +0100

    tekst

---
+++ cpp/tekst.cpp
@@ -0,0 +1,34 @@
+/*
+ * tekst.cpp
+ * Copyright 2019  <>
+ */
+
+
+#include <iostream>
+
+using namespace std;
+
+#define ROZMIAR 100
+
+int main(int argc, char **argv)
+{
+    char tekst[ROZMIAR];
+    int i;
+    int j;
+    int k = 1;
+    cout<<"Podaj zdanie: "<<endl;
+    cin.getline(tekst, ROZMIAR);
+    for (i = 0; i < ROZMIAR ; i++){
+        cout << tekst[i];
+        }
+    for (int j=0;j<ROZMIAR;j++){
+    if (tekst[j]==' '&&tekst[j] != '\0'){
+        k++;
+    }
+    }
+    cout << endl;
+    cout<<"Ilosc slow w zdaniu ""'"<<tekst<<"'"" wynosi "<<k<<endl;
+
+
+    return 0;
+}
