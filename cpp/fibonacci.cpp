dc96a8a26dcc2551aae38dea7ff3fe3591e46435
Bulg0t <michal.potocki@lo1.sandomierz.pl>
2019-11-25 10:35:52 +0100

    stosowny rozdział

--- cpp/fibonacci.cpp
+++ cpp/fibonacci.cpp
@@ -6,28 +6,32 @@
 #include <iostream>
 using namespace std;
 
-int fibonacci_it(int n){
-	int a = 0; //Fn-1
-	int b = 1; //Fn-2
-	int wynik = 0;
-	if (n == 0) return 0;
-	if (n == 1) return 1;
-	for(int i = 0; i<n; i++){
-		wynik = a + b; //1//2//3
-		b = a; //1//1//2
-		a = wynik; //1//2//3
-	}
-	return wynik;
-	}
+int fibonacci_it(int n) {
+    int a = 0; // Fn-1
+    int b = 1; // Fn-2
+    int wynik = 0;
+    if (n == 0) return 0;
+    if (n == 1) return 1;
+    for (int i=2; i<n; i++) {
+        wynik = a + b; //1//2//3
+        b = a; //1//1//2
+        a = wynik; //1//2//3
+        
+    }
+    return wynik;
+}
 
 int main(int argc, char **argv)
 {
-	int r;
-	cout << "Podaj ile liczb ciągu wypisać: ";
-	cin >> r;
-	for(int i = 0; i < r+1; i++){
-	cout << i <<": fib(" << fibonacci_it(i - 2) << ", " << fibonacci_it(i - 1) << ") = " << fibonacci_it(i) << endl;
-	}
-	return 0;
+    cout << fibonacci_it(0) << endl;
+    cout << fibonacci_it(1) << endl;
+    
+    for (int i=2; i < 20; i++) {
+        cout << i << ": fib(" << fibonacci_it (i-2)
+             << ", " << fibonacci_it (i-1) << ") = "
+             << fibonacci_it (i)<< "\t"
+             << float(fibonacci_it(i))/float(fibonacci_it(i-1)) << endl;
+    }
+    return 0;
 }
 
---
+++ cpp/reszta.cpp
@@ -0,0 +1,13 @@
+/*
+ * reszta.cpp
+ */
+
+
+#include <iostream>
+using namespace std;
+int main(int argc, char **argv)
+{
+	int b;
+	return 0;
+}
+
