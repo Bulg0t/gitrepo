c62613c213c17edba65c6f7ef36f9df5389e7de5
Bulg0t <michal.potocki@lo1.sandomierz.pl>
2019-11-27 22:58:39 +0100

    resztoonia

--- cpp/reszta.cpp
+++ cpp/reszta.cpp
@@ -5,9 +5,31 @@
 
 #include <iostream>
 using namespace std;
-int main(int argc, char **argv)
+
+int main(int argc, char *argv[])
 {
-	int b;
-	return 0;
+int B[8]={200, 100, 50, 20, 10, 5, 2, 1};
+int R,K, i;
+
+cout << "Podaj reszte do wyplacenia: ";
+cin >> R;
+
+i=0;
+while (R>0)
+{
+if (R >= B[i])
+{
+K=R / B[i];
+R=R-(B[i]*K);
+if (B[i] >= 10){
+cout << K << " banknotow " << B[i] <<" z³"<<endl;
+}
+else
+    cout << K << " monet " << B[i] <<" z³"<<endl;
+}
+i++;
 }
 
+
+return 0;
+}
