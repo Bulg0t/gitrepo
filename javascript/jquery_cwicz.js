8f7c0290a0314e6b14cf08761da3e9cbb6db3dda
Bulg0t <michal.potocki@lo1.sandomierz.pl>
2020-02-21 10:34:33 +0100

    bekground

--- javascript/jquery_cwicz.js
+++ javascript/jquery_cwicz.js
@@ -2,6 +2,19 @@ $(document).ready(function(){
 	$(".wynik").text("Nowa treść")
 	console.log($("p").text());
 	$("#przycisk").click(function(){
-		;
+		var vnk = parseInt($('#vnk').val());
+		var nk = parseInt($('#nk').val());
+		var R = vnk / nk * 100;
+		var div2 = '<div class="wynik">'+R+'</div>';
+		$("div.wynik").after(div2);
 	});
+			$('input').mouseover(function(){
+				$(this).addClass("tloBlue");
+			});
+			$('input').mouseleave(function(){
+				$(this).removeClass("tloBlue");
+			});
+			// podczas opuszczania myszy z obszaru input
+			//przywróć poprzedni kolor tła
+
 	});
