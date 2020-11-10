c8c36208ca3203e892d26ab0bba417232180771c
Bulg0t <michal.potocki@lo1.sandomierz.pl>
2020-11-10 13:02:23 +0100

    newsphp

---
+++ framework-4.0.4/app/Models/NewsModel.php
@@ -0,0 +1,25 @@
+<?php namespace App\Models;
+
+use CodeIgniter\Model;
+
+class NewsModel extends Model
+{
+    protected $table = 'news';
+
+    public function getNews($slug = false)
+    {
+        if ($slug === false)
+        {
+            return $this->findAll();
+        }
+
+        return $this->asArray()
+        ->where(['slug' => $slug])
+        ->first();
+    }
+    public function getTitles()
+    {
+        $query = $this->db->query('SELECT title, slug FROM news');
+        return $query->getResultArray();
+    }
+}
